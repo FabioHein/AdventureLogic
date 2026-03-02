309859e500601a5e7d1483f0fae7d04f5a0567eb
FabioHein <fabio.heinreichsberger@gmail.com>
2026-03-02 20:02:42 +0100

    Hero klasse erstellt und State + State wechsel eingebaut.

---
+++ src/Core/Hero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AdventureLogic.src.Enums;
+
+namespace AdventureLogic.src.Core
+{
+    public class Hero
+    {
+
+        private string _heroName;
+        private State _currentState;
+
+        public Hero(string name, State state)
+        {
+            _heroName = name;
+            _currentState = state;
+        }
+
+        public void StateLog()
+        {
+            Console.WriteLine($"Der Held ist in dem State: {_currentState}");
+        }
+        public void ChangeState(State newState)
+        {
+            if(_currentState == newState)
+            {
+                Console.WriteLine($"{_heroName} ist bereits in dieser State ({_currentState})");
+                return;
+            }
+                Console.WriteLine($"{_heroName} hat von {_currentState} zu {newState} gewechselt.");
+                _currentState = newState;
+        }
+
+    }
+}
