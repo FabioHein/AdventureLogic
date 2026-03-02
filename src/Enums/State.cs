59777b7d72ee9fc35139a0821342805ddcb1447e
FabioHein <fabio.heinreichsberger@gmail.com>
2026-03-02 19:45:08 +0100

    State Enum hinzugefügt.

---
+++ src/Enums/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureLogic.src.Enums
+{
+    public enum State
+    {
+        Idle,
+        Walk,
+        Run,
+        Jump
+    }
+}
